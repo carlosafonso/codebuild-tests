a23e92d155164e26587a6e7057ea5bec619bc6a8
Carlos Afonso Perez <cafonsop@amazon.com>
2018-10-10 00:00:46 +0200

    Params and values

--- create_or_update_pipeline.sh
+++ create_or_update_pipeline.sh
@@ -6,7 +6,14 @@ if [ $? -ne 0 ]; then
 	echo "Pipeline does not exist, so it will be created now"
 	aws cloudformation create-stack \
 		--stack-name=${CAFONSOP_PIPELINE_STACK} \
-		--template-body=file://$PWD/pipeline.yaml
+		--template-body=file://$PWD/pipeline.yaml \
+		--parameters='[
+			{"ParameterKey": "GitHubUser", "ParameterValue": "$CAFONSOP_GITHUB_USER"},
+			{"ParameterKey": "GitHubRepo", "ParameterValue": "$CAFONSOP_GITHUB_REPO"},
+			{"ParameterKey": "GitHubBranch", "ParameterValue": "$CAFONSOP_PIPELINE_STACK"},
+			{"ParameterKey": "Cluster", "ParameterValue": "$CAFONSOP_ECS_CLUSTER"},
+			{"ParameterKey": "Service", "ParameterValue": "$CAFONSOP_ECS_SERVICE"}
+		]'
 else
 	echo "Pipeline exists, so it will be updated"
 	aws cloudformation update-stack \
